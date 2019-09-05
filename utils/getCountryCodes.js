22d2ce1205080c537da5c8f0c56456428d783fe9
“juliantreweeke” <juliantreweeke@gmail.com>
2019-09-06 00:23:04 +0300

    got skyscanner city codes

---
+++ utils/getCountryCodes.js
@@ -0,0 +1,46 @@
+// this was used in the main App.vue file in mounted
+// as a hacky way to make list of every city's sky scanner cityId codes which are not available online
+
+const getCodes = function () {
+    var countries = require("../utils/countries.json"); //with path
+    var cities = [];
+    countries.forEach((country, index) => {
+        fetch(
+                `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${country.name}`, {
+                    method: "GET",
+                    headers: {
+                        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+                        "x-rapidapi-key": config.RAPIDAPI_SKY_KEY
+                    }
+                }
+            )
+            .then(response => {
+                response.json().then(data => {
+                    data.Places.forEach(city => {
+                        if (city.CityId === "-sky") {
+                            return;
+                        }
+                        cities.push({
+                            id: city.CityId,
+                            country: city.CountryName
+                        });
+                        console.log(city.CountryName);
+                        if (city.CountryName === "Zimbabwe") {
+                            let dataStr = JSON.stringify(cities);
+                            let dataUri =
+                                "data:application/json;charset=utf-8," +
+                                encodeURIComponent(dataStr);
+                            let exportFileDefaultName = "data.json";
+                            let linkElement = document.createElement("a");
+                            linkElement.setAttribute("href", dataUri);
+                            linkElement.setAttribute("download", exportFileDefaultName);
+                            linkElement.click();
+                        }
+                    });
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+}
